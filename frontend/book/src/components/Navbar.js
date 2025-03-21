4e9f3deb802b7d25f9c95451b127a9630c7fb05e
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-22 00:05:40 +0545

    Navbar

--- frontend/book/src/components/Navbar.js
+++ frontend/book/src/components/Navbar.js
@@ -25,19 +25,19 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="bg-zinc-800 text-white px-4 py-3 flex justify-between">
+    <div className="bg-zinc-800 text-white px-4 py-5 flex justify-between">
       <div>
         <h1 className="text-xl font-semibold">Book Store</h1>
       </div>
-      <div >
-        <div className="nav-link-book flex gap-5">
+      <div className="nav-link-book flex items-center gap-5">
+        <div className="flex gap-5">
         {links.map((items,i)=>(
           <div className="hover:text-blue-400 transition-all duration-500 cursor-pointer" key={i}>{items.title}</div>
         ))}
         </div>
        <div className="flex gap-4">
-        <button className="px-4 py-1 border border-blue-500">Login</button>
-        <button className= "px-4 py-1 bg-white text-black rounded">SignUp</button>
+        <button className="px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-black transition-all duration-500">Login</button>
+        <button className= "px-4 py-1 bg-blue-500 text-white rounded hover:bg-white hover:text-black">SignUp</button>
        </div>
       </div>
     </div>
