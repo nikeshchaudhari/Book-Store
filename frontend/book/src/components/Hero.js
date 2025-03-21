c225fc1bf3c85d61f15dad61de302b281bf86d7a
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-22 00:06:11 +0545

    Hero

---
+++ frontend/book/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-[75vh] flex'>
+     <div className='w-1/3'>
+
+     </div>
+     <div className='w-1/3'>
+
+     </div>
+    </div>
+  )
+}
+
+export default Hero
