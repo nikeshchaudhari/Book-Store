1ff43c43de1e07174d37143bdec8517f069c016f
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-24 23:47:51 +0545

    Update to all

--- frontend/book/src/components/Hero.js
+++ frontend/book/src/components/Hero.js
@@ -1,8 +1,9 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] flex">
+    <div className="h-screen md:h-[75vh] flex flex-col md:flex-row items-center">
       <div className=" w-full lg:w-1/2 flex  flex-col items-center lg:items-start justify-center">
         <h1 className=" text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
           Discover Your Next Great Book
@@ -12,12 +13,12 @@ const Hero = () => {
           collection of books
         </p>
         <div className="mt-8 py-3">
-          <button className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
+          <Link to = "/all-books" className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
             Discover Books
-          </button>
+          </Link>
         </div>
       </div>
-      <div className="w-full lg:w1/2 h-auto lg-h-[100%] flex items-center justify-center">
+      <div className=" md:w-full lg:w1/2 my-48 max-h-20 lg-h-[100%] flex items-center justify-center">
       <img src={require("../components/images/pngegg (3).png")} alt="hero image" className="w-4/6 h-[450px] "></img>
       </div>
     </div>
