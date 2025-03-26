6a80bb387172fd4fa71fc37ac4f47ebf05bd8edb
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-26 22:48:25 +0545

    update

---
+++ frontend/book/src/components/Recentedit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Recentedit = () => {
+  return (
+    <div className='mt-8 px-4'>
+      <div className='text-3xl text-yellow-200'>
+        <h4>Recently Edit Book</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Recentedit
